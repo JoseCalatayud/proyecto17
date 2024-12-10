59ac0208d65b22c43c0bbb02e581e870fc1f7dfe
curso05 <notiene@notiene.es>
2024-12-10 11:14:08 +0100

    Terminada

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -9,7 +9,7 @@ $(function () {
     $('.respuesta').append($('<input id="respuesta" type="text">'))
 
     const listarTodos = () => {
-        $.get("https://my-json-server.typicode.com/desarrollo-seguro/dato/solicitudes", function (data) {
+        $.get("https://my-json-server.typicode.com/desarrollo-seguro/proyecto17/solicitudes", function (data) {
             $('input').val('')
             $('input').val('OK')
         }
@@ -17,60 +17,84 @@ $(function () {
         )
     }
     function listarUno(id) {
-        $.get(`https://my-json-server.typicode.com/desarrollo-seguro/dato/solicitudes/${id}`)
-            .done(function (id) {
+        $.get(`https://my-json-server.typicode.com/desarrollo-seguro/proyecto17/solicitudes/${id}`)
+
+            .done(function (data) {
                 $('input').val('')
-                $('input').val('OK')
+                $('input').val('OK '+ data)
             })
             .fail((e) => {
                 $('input').val(e.status)
             })
 
     }
+    // con promesas
     function guardar() {
-        let datos = [{
-            "id":3,
+        let datos = {
             "nombre": "jose",
             "apellido": "sinApellido"
-        }]
+        }
         $.ajax({
-            url: "https://my-json-server.typicode.com/desarrollo-seguro/dato/solicitudes", // URL del servidor
-            method: "POST", // Método HTTP
+            url: "https://my-json-server.typicode.com/desarrollo-seguro/proyecto17/solicitudes/", // URL del servidor
+            type: "POST", // Método HTTP
             data: JSON.stringify(datos), // Envía el JSON plano
-            contentType: "application/json", // Especifica que el contenido es JSON
-            success: function (respuesta) {
+            contentType: "application/json" // Especifica que el contenido es JSON            
+        }).then(
+            (response) => {
                 $('input').val('')
                 $('input').val(`Registro guardado`)
             },
-            error: function (error) {
+            (error) => {
                 $('input').val('')
-                $('input').val(error.responseText)
+                $('input').val(error.statusText)
+            }
+        )
+
+
+    }
+    //sin promesas
+    function modificar(id) {
+        let datos = {
+            "id": 1,
+            "nombre": "jose",
+            "apellido": "Calatayud"
+        }
+        $.ajax({
+            url: `https://my-json-server.typicode.com/desarrollo-seguro/proyecto17/solicitudes/${id}`, // URL del servidor
+            type: "PUT", // Método HTTP
+            data: JSON.stringify(datos), // Envía el JSON plano
+            contentType: "application/json", // Especifica que el contenido es JSON
+            success: (respuesta) => {
+                $('input').val('')
+                $('input').val('Registro modificado')
+            },
+            error: (error) => {
+                $('input').val('')
+                $('input').val('Error en la modificacion' + error)
             }
         });
-        
-    }
-    function modificar() {
-        
     }
     function borrar(id) {
         $.ajax({
-            url: `https://my-json-server.typicode.com/desarrollo-seguro/dato/solicitudes/${id}`,
-            method: "DELETE",
+            url: `https://my-json-server.typicode.com/desarrollo-seguro/proyecto17/solicitudes/${id}`,
+            type: "DELETE",
+
         }
         )
-            .done(() => {
+            .done((data) => {
                 $('input').val('')
-                $('input').val(`Registro ${id} borrado`)
+                $('input').val(`Registro ${id} borrado ${data}`)
             })
             .fail((e) => {
-                $('input').val('Fallo en el borrado ' + e.status)
+                $('input').val('Fallo en el borrado ' + e.status + e.statusText)
             })
     }
 
     $('#listar').on('click', listarTodos)
-    $('#listarID').on('click', () => listarUno(27))
+    $('#listarID').on('click', () => listarUno(1))
     $('#guardar').on('click', () => guardar())
-    $('#borrar').on('click', () => borrar(2))
+    $('#modificar').on('click', () => modificar(1))
+    $('#borrar').on('click', () => borrar(9))
 
 
 
